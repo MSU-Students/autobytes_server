453ded9cc6adee9e8f84a85baa852e3ccc4871f4
usman12 <usman12strygwyr@gmail.com>
2021-07-06 22:27:46 +0800

    create clearance entity with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { ClearanceController } from './clearance/clearance.controller';
+import { ClearanceService } from './clearance/clearance.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{name: User.name, schema: UserSchema }]),
     MongooseModule.forRoot('mongodb://localhost/autobytes')
   ],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  controllers: [AppController, UserController, ClearanceController],
+  providers: [AppService, UserService, ClearanceService],
 })
 export class AppModule {}

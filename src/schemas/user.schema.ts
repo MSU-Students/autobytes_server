8a8174869a1cc61825d0e363cea3050f5d298adf
usman12 <usman12strygwyr@gmail.com>
2021-07-07 20:51:28 +0800

    user schema + interface

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,13 +1,31 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import { Document } from 'mongoose';
+import { IUser } from 'src/interfaces/user.interface';
 
 export type UserDocument = User & Document;
 
 @Schema()
-export class User {
+export class User implements IUser {
+
+    @ApiProperty()
     @Prop({required:true})
     firstName: string;
-    @Prop()
+
+    @ApiProperty()
+    @Prop({required:true})
     lastName: string;
+
+    @ApiProperty()
+    @Prop({required:true})
+    email: string;
+
+    @ApiProperty()
+    @Prop({required:true})
+    course: string;
+
+    @ApiProperty()
+    @Prop({required:true})
+    idNumber: string;
 }
 export const UserSchema = SchemaFactory.createForClass(User);

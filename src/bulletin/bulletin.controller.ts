05034c7c5ea0f2da3365cc1d0301696528161cd7
usman12 <usman12strygwyr@gmail.com>
2021-07-08 16:55:21 +0800

    bulletin CRUD

--- src/bulletin/bulletin.controller.ts
+++ src/bulletin/bulletin.controller.ts
@@ -1,18 +1,44 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Delete, Query } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Bulletin } from 'src/schemas/bulletin.schema';
 import { BulletinService } from './bulletin.service';
 
 @Controller('bulletin')
 export class BulletinController {
     constructor(private bulletinService: BulletinService) { }
-    @Post()
+    @ApiBody({ type: Bulletin })
+    @ApiOperation({ summary: 'Add new Bulletin', operationId: 'AddBulletin' })
+    @ApiResponse({ status: 200, type: Bulletin })
+    @Post('/create')
     create(@Body() bulletin: Bulletin) {
-        console.log(bulletin);
         return this.bulletinService.create(bulletin);
     }
 
-    @Get()
+    @ApiOperation({ summary: 'Get all Bulletin', operationId: 'GetBulletin' })
+    @ApiResponse({ status: 200, type: Bulletin })
+    @Get('/all')
     async findAll() {
         return this.bulletinService.findAll();
+    }
+
+    @ApiOperation({ summary: 'Get Bulletin by id', operationId: 'GetBulletin' })
+    @ApiResponse({ status: 200, type: Bulletin })
+    @Get('id')
+    async findById(@Query('id') id: string) {
+        return await this.bulletinService.findById(id);
+    }
+
+    @ApiOperation({ summary: 'Update Bulletin by id', operationId: 'UpdateBulletin' })
+    @ApiResponse({ status: 200, type: Bulletin })
+    @Put('/update')
+    async update(@Query('id') id: string, @Body() Bulletin: Bulletin) {
+        return await this.bulletinService.update(id, Bulletin);
+    }
+
+    @ApiOperation({ summary: 'Delete Bulletin by id', operationId: 'DeleteBulletin' })
+    @ApiResponse({ status: 200, type: Bulletin })
+    @Delete('/delete')
+    async delete(@Query('id') id: string) {
+        return await this.bulletinService.delete(id);
     }
 }
--- src/bulletin/bulletin.service.ts
+++ src/bulletin/bulletin.service.ts
@@ -5,13 +5,24 @@ import { Bulletin, BulletinDocument } from 'src/schemas/bulletin.schema';
 
 @Injectable()
 export class BulletinService {
-    constructor(@InjectModel(Bulletin.name) private userModel: Model<BulletinDocument>){}
+    constructor(@InjectModel(Bulletin.name) private bulletinModel: Model<BulletinDocument>) { }
     create(bulletin: Bulletin) {
-        const createdBulletin = new this.userModel(bulletin);
+        const createdBulletin = new this.bulletinModel(bulletin);
         return createdBulletin.save();
     }
     async findAll(): Promise<Bulletin[]> {
-        return this.userModel.find().exec();
+        return this.bulletinModel.find().exec();
     }
-    
+    async findById(id: string): Promise<Bulletin> {
+        return this.bulletinModel.findById(id).exec();
+    }
+
+    async update(id: string, Bulletin: Bulletin): Promise<any> {
+        return await this.bulletinModel.findByIdAndUpdate(id, Bulletin, {
+            new: true,
+        });
+    }
+    async delete(id: string): Promise<any> {
+        return await this.bulletinModel.findByIdAndRemove(id);
+    }
 }

862a0268a159aea8b860187d07f83a4fe3b2f5b9
usman12 <usman12strygwyr@gmail.com>
2021-07-08 16:55:00 +0800

    attendance CRUD

--- src/attendance/attendance.controller.ts
+++ src/attendance/attendance.controller.ts
@@ -1,18 +1,44 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Delete, Query } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Attendance } from 'src/schemas/attendance.schema';
 import { AttendanceService } from './attendance.service';
 
 @Controller('attendance')
 export class AttendanceController {
     constructor(private attendanceService:AttendanceService) {}
-    @Post()
-    create(@Body()attendance: Attendance) {
-        console.log(attendance);
-        return this.attendanceService.create(attendance);
-    }
+    @ApiBody({ type: Attendance })
+  @ApiOperation({ summary: 'Add new Attendance', operationId: 'AddAttendance' })
+  @ApiResponse({ status: 200, type: Attendance })
+  @Post('/create')
+  create(@Body() Attendance: Attendance) {
+    return this.attendanceService.create(Attendance);
+  }
 
-    @Get()
-    async findAll() {
-        return this.attendanceService.findAll();
-    }
+  @ApiOperation({ summary: 'Get all Attendance', operationId: 'GetAttendance' })
+  @ApiResponse({ status: 200, type: Attendance })
+  @Get('/all')
+  async findAll() {
+    return this.attendanceService.findAll();
+  }
+
+  @ApiOperation({ summary: 'Get Attendance by id', operationId: 'GetAttendance' })
+  @ApiResponse({ status: 200, type: Attendance })
+  @Get('id')
+  async findById(@Query('id') id: string) {
+    return await this.attendanceService.findById(id);
+  }
+
+  @ApiOperation({ summary: 'Update Attendance by id', operationId: 'UpdateAttendance' })
+  @ApiResponse({ status: 200, type: Attendance })
+  @Put('/update')
+  async update(@Query('id') id: string, @Body() Attendance: Attendance) {
+    return await this.attendanceService.update(id, Attendance);
+  }
+
+  @ApiOperation({ summary: 'Delete Attendance by id', operationId: 'DeleteAttendance' })
+  @ApiResponse({ status: 200, type: Attendance })
+  @Delete('/delete')
+  async delete(@Query('id') id: string) {
+    return await this.attendanceService.delete(id);
+  }
 }
--- src/attendance/attendance.service.ts
+++ src/attendance/attendance.service.ts
@@ -5,12 +5,24 @@ import { Attendance, AttendanceDocument } from 'src/schemas/attendance.schema';
 
 @Injectable()
 export class AttendanceService {
-    constructor(@InjectModel(Attendance.name) private attendanceModel: Model<AttendanceDocument>){}
+    constructor(@InjectModel(Attendance.name) private attendanceModel: Model<AttendanceDocument>) { }
     create(attendance: Attendance) {
         const createdAttendance = new this.attendanceModel(attendance);
         return createdAttendance.save();
     }
     async findAll(): Promise<Attendance[]> {
         return this.attendanceModel.find().exec();
+    }
+    async findById(id: string): Promise<Attendance> {
+        return this.attendanceModel.findById(id).exec();
+    }
+
+    async update(id: string, Attendance: Attendance): Promise<any> {
+        return await this.attendanceModel.findByIdAndUpdate(id, Attendance, {
+            new: true,
+        });
+    }
+    async delete(id: string): Promise<any> {
+        return await this.attendanceModel.findByIdAndRemove(id);
     }
 }

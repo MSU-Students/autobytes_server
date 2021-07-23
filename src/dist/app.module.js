d9e697f4b41ca0cb5f700e2b58f3394cf9122e00
usman12 <usman12strygwyr@gmail.com>
2021-07-24 00:22:52 +0800

    auth updates

--- src/dist/app.module.js
+++ src/dist/app.module.js
@@ -35,17 +35,22 @@ var media_service_1 = require("./media/media.service");
 var students_schema_1 = require("./schemas/students.schema");
 var students_controller_1 = require("./students/students.controller");
 var students_service_1 = require("./students/students.service");
+var auth_module_1 = require("./user/auth.module");
+var config_1 = require("@nestjs/config");
+var configuration_1 = require("./config/configuration");
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         common_1.Module({
             imports: [
+                config_1.ConfigModule.forRoot({
+                    isGlobal: true,
+                    load: [configuration_1["default"]],
+                    envFilePath: ['.env.dev.local', '.env.dev', '.env.prod']
+                }),
                 mongoose_1.MongooseModule.forFeature([{
                         name: attendance_schema_1.Attendance.name, schema: attendance_schema_1.AttendanceSchema
-                    },
-                    {
-                        name: user_schema_1.User.name, schema: user_schema_1.UserSchema
                     },
                     {
                         name: bulletin_schema_1.Bulletin.name, schema: bulletin_schema_1.BulletinSchema
@@ -64,13 +69,17 @@ var AppModule = /** @class */ (function () {
                     },
                     {
                         name: students_schema_1.Students.name, schema: students_schema_1.StudentsSchema
-                    }]),
+                    }, {
+                        name: user_schema_1.User.name, schema: user_schema_1.UserSchema
+                    }
+                ]),
                 mongoose_1.MongooseModule.forRoot('mongodb://localhost/autobytes', {
                     useFindAndModify: false
                 }),
+                auth_module_1.AuthModule
             ],
-            controllers: [app_controller_1.AppController, user_controller_1.UserController, attendance_controller_1.AttendanceController, bulletin_controller_1.BulletinController, clearance_controller_1.ClearanceController, records_controller_1.RecordsController, archived_controller_1.ArchivedController, media_controller_1.MediaController, students_controller_1.StudentsController],
-            providers: [app_service_1.AppService, user_service_1.UserService, attendance_service_1.AttendanceService, bulletin_service_1.BulletinService, clearance_service_1.ClearanceService, records_service_1.RecordsService, archived_service_1.ArchivedService, media_service_1.MediaService, students_service_1.StudentsService]
+            controllers: [app_controller_1.AppController, attendance_controller_1.AttendanceController, bulletin_controller_1.BulletinController, clearance_controller_1.ClearanceController, records_controller_1.RecordsController, archived_controller_1.ArchivedController, media_controller_1.MediaController, students_controller_1.StudentsController, user_controller_1.UserController],
+            providers: [app_service_1.AppService, attendance_service_1.AttendanceService, bulletin_service_1.BulletinService, clearance_service_1.ClearanceService, records_service_1.RecordsService, archived_service_1.ArchivedService, media_service_1.MediaService, students_service_1.StudentsService, user_service_1.UserService]
         })
     ], AppModule);
     return AppModule;
